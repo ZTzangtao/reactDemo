67eb169998eb05006b4c7ac115bb17a1d7541e34
zangtao <TZANG1@ford.com>
2022-11-09 23:38:39 +0800

    react render-props 步骤

---
+++ react_basic_syntax/my-app/src/js/HighLevelComponent/mouseComponent.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/**
+    render props 模式
+ */
+
+// 创建Mouse组件
+class Mouse extends React.Component {
+    // 鼠标位置state
+    static = {
+        x: 0,
+        y: 0
+    }
+
+    // 鼠标移动事件的事件处理程序
+    handleMouseMove = (e) => {
+        this.state({
+            x: e.clientX,
+            y: e.clientY
+        })
+    }
+
+    // 监听鼠标移动事件
+    componentDidMount() {
+        window.addEventListener('mousemove', this.handleMouseMove)
+    }
+
+    render() {
+        return null
+    }
+}
+export default Mouse
